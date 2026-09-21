7fb028aa45924133a1f06071e1946a917cd0594d
nideeshnagaraj-cmyk <nideeshnagaraj@gmail.com>
2026-09-21 18:55:43 +0530

    Delete Medium directory

--- Medium/0050_Pow(x, n).py
+++
@@ -1,4 +0,0 @@
-import math
-def myPow(x,n):
-    return math.pow(x,n)
-print(myPow(x=-1,n=2147483647))
--- Medium/2396_Strictly_Palindromic_Number.py
+++
@@ -1,8 +0,0 @@
-def isStrictlyPalindromic(n):
-    return False
-print(isStrictlyPalindromic(9))
-'''
-An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive),
-the string representation of the integer n in base b is palindromic.
-Any number will not satisfy this condition, so for all results it is false
-'''
